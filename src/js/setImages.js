0d1fec5bdc4f5ea730375e1d951ee363a8a62158
Tymoteusz Huba <tymoteuszhuba@MacBook-Air-Tymoteusz.local>
2024-12-30 20:50:01 +0100

    added styles in sass

--- src/js/setImages.js
+++ src/js/setImages.js
@@ -1,6 +1,6 @@
 import logotype from '../images/logotype.png';
 
-const logoElement = document.querySelector('.logo img');
+const logoElement = document.querySelector('.nav-logo img');
 logoElement.src = logotype;
 
 export {logoElement};
