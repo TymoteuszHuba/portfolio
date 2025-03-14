73429bb0943a8f568047b9546165cf0965be45c7
Tymoteusz Huba <tymoteuszhuba@mac.home>
2025-03-14 10:09:42 +0100

    Updates

---
+++ src_locales_en_json.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkportfolio_ver_2"] = self["webpackChunkportfolio_ver_2"] || []).push([["src_locales_en_json"],{
+
+/***/ "./src/locales/en.json":
+/*!*****************************!*\
+  !*** ./src/locales/en.json ***!
+  \*****************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"nav":{"about":"About me","skills":"Skills","projects":"Projects"},"header":{"static":"Time to ___","words":["WEBSITES","ANALYSIS","DEVELOPMENT","FUTURE"]},"about":{"greeting":"Hello!","title":"My name is","special":"Front-end Developer / Data Analyst"}}');
+
+/***/ })
+
+}]);
